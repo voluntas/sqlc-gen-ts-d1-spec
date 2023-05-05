81686496c523ca97ba2046f90930eae82f18af36
voluntas <nakai@shiguredo.jp>
2023-05-05 13:48:16 +0900

    sqlc-gen-typescript-d1 を利用したコードに置き換える

--- src/gen/sqlc/querier.ts
+++ src/gen/sqlc/querier.ts
@@ -1,141 +1,165 @@
-// これはまだ sqlc-gen-typescript-d1 で自動生成したコードではありません
-
-import { D1Database, D1Result } from '@cloudflare/workers-types'
+import {D1Database, D1Result} from "@cloudflare/workers-types/2022-11-30"
 
 const getAccountQuery = `-- name: GetAccount :one
-SELECT pk, id, display_name, email FROM account WHERE id = ?1`
+SELECT pk, id, display_name, email
+FROM account
+WHERE id = ?1`;
 
 export type GetAccountParams = {
-  id: string
-}
+  id: string;
+};
 
 export type GetAccountRow = {
-  pk: bigint
-  id: string
-  displayName: string
-  email: string | null
-}
+  pk: bigint;
+  id: string;
+  displayName: string;
+  email: string | null;
+};
 
 type RawGetAccountRow = {
-  pk: bigint
-  id: string
-  display_name: string
-  email: string | null
-}
+  pk: bigint;
+  id: string;
+  display_name: string;
+  email: string | null;
+};
 
 export async function getAccount(
   d1: D1Database,
-  args: GetAccountParams,
+  args: GetAccountParams
 ): Promise<GetAccountRow | null> {
   return await d1
     .prepare(getAccountQuery)
     .bind(args.id)
     .first<RawGetAccountRow | null>()
-    .then((raw: RawGetAccountRow | null) =>
-      raw
-        ? {
-            pk: raw.pk,
-            id: raw.id,
-            displayName: raw.display_name,
-            email: raw.email,
-          }
-        : null,
-    )
+    .then((raw: RawGetAccountRow | null) => raw ? {
+      pk: raw.pk,
+      id: raw.id,
+      displayName: raw.display_name,
+      email: raw.email,
+    } : null);
 }
 
 const listAccountsQuery = `-- name: ListAccounts :many
-SELECT pk, id, display_name, email FROM account`
+SELECT pk, id, display_name, email
+FROM account`;
+
+export type ListAccountsParams = {
+};
 
 export type ListAccountsRow = {
-  pk: bigint
-  id: string
-  displayName: string
-  email: string | null
-}
+  pk: bigint;
+  id: string;
+  displayName: string;
+  email: string | null;
+};
 
 type RawListAccountsRow = {
-  pk: bigint
-  id: string
-  display_name: string
-  email: string | null
-}
+  pk: bigint;
+  id: string;
+  display_name: string;
+  email: string | null;
+};
 
-export async function listAccounts(d1: D1Database): Promise<D1Result<ListAccountsRow>> {
+export async function listAccounts(
+  d1: D1Database,
+  args: ListAccountsParams
+): Promise<D1Result<ListAccountsRow>> {
   return await d1
     .prepare(listAccountsQuery)
     .all<RawListAccountsRow>()
-    .then((r: D1Result<RawListAccountsRow>) => {
-      return {
-        ...r,
-        results: r.results
-          ? r.results.map((raw: RawListAccountsRow) => {
-              return {
-                pk: raw.pk,
-                id: raw.id,
-                displayName: raw.display_name,
-                email: raw.email,
-              }
-            })
-          : undefined,
-      }
-    })
+    .then((r: D1Result<RawListAccountsRow>) => { return {
+      ...r,
+      results: r.results ? r.results.map((raw: RawListAccountsRow) => { return {
+        pk: raw.pk,
+        id: raw.id,
+        displayName: raw.display_name,
+        email: raw.email,
+      }}) : null,
+    }});
 }
 
 const createAccountQuery = `-- name: CreateAccount :exec
 INSERT INTO account (id, display_name, email)
-VALUES (?1, ?2, ?3)`
+VALUES (?1, ?2, ?3)`;
 
 export type CreateAccountParams = {
-  id: string
-  displayName: string
-  email: string
-}
+  id: string;
+  displayName: string;
+  email: string | null;
+};
 
-export async function createAccount(d1: D1Database, args: CreateAccountParams): Promise<D1Result> {
-  return await d1.prepare(createAccountQuery).bind(args.id, args.displayName, args.email).run()
+export type CreateAccountRow = {
+};
+
+export async function createAccount(
+  d1: D1Database,
+  args: CreateAccountParams
+): Promise<D1Result<CreateAccountRow>> {
+  return await d1
+    .prepare(createAccountQuery)
+    .bind(args.id, args.displayName, args.email)
+    .run<CreateAccountRow>();
 }
 
 const updateAccountDisplayNameQuery = `-- name: UpdateAccountDisplayName :one
 UPDATE account
 SET display_name = ?1
 WHERE id = ?2
-RETURNING pk, id, display_name, email`
+RETURNING pk, id, display_name, email`;
 
 export type UpdateAccountDisplayNameParams = {
-  displayName: string
-  id: string
-}
+  displayName: string;
+  id: string;
+};
 
 export type UpdateAccountDisplayNameRow = {
-  pk: bigint
-  id: string
-  displayName: string
-  email: string | null
-}
+  pk: bigint;
+  id: string;
+  displayName: string;
+  email: string | null;
+};
 
 type RawUpdateAccountDisplayNameRow = {
-  pk: bigint
-  id: string
-  display_name: string
-  email: string | null
-}
+  pk: bigint;
+  id: string;
+  display_name: string;
+  email: string | null;
+};
 
 export async function updateAccountDisplayName(
   d1: D1Database,
-  args: UpdateAccountDisplayNameParams,
+  args: UpdateAccountDisplayNameParams
 ): Promise<UpdateAccountDisplayNameRow | null> {
   return await d1
     .prepare(updateAccountDisplayNameQuery)
     .bind(args.displayName, args.id)
     .first<RawUpdateAccountDisplayNameRow | null>()
-    .then((raw: RawUpdateAccountDisplayNameRow | null) =>
-      raw
-        ? {
-            pk: raw.pk,
-            id: raw.id,
-            displayName: raw.display_name,
-            email: raw.email,
-          }
-        : null,
-    )
+    .then((raw: RawUpdateAccountDisplayNameRow | null) => raw ? {
+      pk: raw.pk,
+      id: raw.id,
+      displayName: raw.display_name,
+      email: raw.email,
+    } : null);
 }
+
+const deleteAccountQuery = `-- name: DeleteAccount :exec
+DELETE FROM account
+WHERE id = ?1`;
+
+export type DeleteAccountParams = {
+  id: string;
+};
+
+export type DeleteAccountRow = {
+};
+
+export async function deleteAccount(
+  d1: D1Database,
+  args: DeleteAccountParams
+): Promise<D1Result<DeleteAccountRow>> {
+  return await d1
+    .prepare(deleteAccountQuery)
+    .bind(args.id)
+    .run<DeleteAccountRow>();
+}
+
