ce6d9473ca78852b7e9611019f15c026abd308d1
voluntas <nakai@shiguredo.jp>
2023-05-06 13:49:32 +0900

    コメント

--- test/index.test.ts
+++ test/index.test.ts
@@ -11,10 +11,9 @@ import { D1Database as miniflareD1Database, D1DatabaseAPI } from '@miniflare/d1'
 test('test 1', async () => {
   const sqliteDb = await createSQLiteDB(':memory:')
   // sqlc で生成したコードを使いたいが D1Database の型がマッチしない問題がある
-  // miniflare が定義している D1Database と
-  // @cloudflare/workers-types/2022 - 11 - 30 が定義している D1Database の型が異なる
-  // そのためそもそも D1 を渡そうとするとエラーになる
-  // なので @cloudflare/workers-types の D1Database 型に無理矢理変換している
+  // miniflare が定義している D1Database と @cloudflare/workers-types が定義している D1Database の型が異なる
+  // そのため D1 を渡そうとするとエラーになる
+  // 解決策として @cloudflare/workers-types の D1Database 型に無理矢理変換している
   const D1 = new miniflareD1Database(new D1DatabaseAPI(sqliteDb)) as D1Database
 
   // TODO: ここは本来 db/schema.sql ファイル読み込むようにする
