2759344c91e7bd7de5cfb404ac6cb682d7fb9169
voluntas <nakai@shiguredo.jp>
2023-10-02 00:16:43 +0900

    コメント

--- test/account.test.ts
+++ test/account.test.ts
@@ -47,6 +47,7 @@ test('account', async () => {
   expect(a2).not.toBeNull()
   expect(a2?.displayName).toBe('Test2')
 
+  // batch のテスト
   const r3 = await D1.batch([
     db.deleteAccount(D1, { id: 'test' }).batch(),
     db.createAccount(D1, { id: 'test', displayName: 'Test', email: 'test@example.com' }).batch(),
