29815f0eba432760c5a8436c24a48b34ccc5a831
voluntas <nakai@shiguredo.jp>
2024-02-24 00:50:19 +0900

    biome 化する

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,30 @@
-import * as db from './gen/sqlc/querier'
+import * as db from "./gen/sqlc/querier";
 
 export interface Env {
-  D1_TEST: D1Database
+  D1_TEST: D1Database;
 }
 
 export default {
-  async fetch(request: Request, env: Env, ctx: ExecutionContext): Promise<Response> {
+  async fetch(
+    request: Request,
+    env: Env,
+    ctx: ExecutionContext,
+  ): Promise<Response> {
     try {
       await db.createOrg(env.D1_TEST, {
-        id: 'example',
-        displayName: 'Example Inc.',
-      })
+        id: "example",
+        displayName: "Example Inc.",
+      });
     } catch (error) {
       // console.log(error)
     }
 
     try {
       await db.createAccount(env.D1_TEST, {
-        id: 'voluntas',
-        displayName: 'V',
-        email: 'v@example.com',
-      })
+        id: "voluntas",
+        displayName: "V",
+        email: "v@example.com",
+      });
     } catch (error) {
       // console.log(error)
     }
@@ -29,49 +33,51 @@ export default {
       await db.createOrgAccount(env.D1_TEST, {
         orgPk: 1,
         accountPk: 1,
-      })
+      });
     } catch (error) {
       // console.log(error)
     }
 
     const r = await env.D1_TEST.prepare(
-      'SELECT \
+      "SELECT \
         account.pk, account.id, account.display_name, account.email, \
         org.pk, org.id, org.display_name \
        FROM account \
        JOIN org_account ON account.pk = org_account.account_pk \
        JOIN org ON org_account.org_pk = org.pk \
-       WHERE org.id = $1 AND account.id = $2;',
+       WHERE org.id = $1 AND account.id = $2;",
     )
-      .bind('example', 'voluntas')
-      .raw()
-    console.log('GetOrgAccountRaw: ', r)
+      .bind("example", "voluntas")
+      .raw();
+    console.log("GetOrgAccountRaw: ", r);
 
-    const r2 = await env.D1_TEST.prepare('INSERT INTO account_log (tag, data) VALUES ($1, $2);')
-      .bind('debug', JSON.stringify({ a: 'b' }))
-      .raw()
-    console.log('CreateAccountLog: ', r2)
+    const r2 = await env.D1_TEST.prepare(
+      "INSERT INTO account_log (tag, data) VALUES ($1, $2);",
+    )
+      .bind("debug", JSON.stringify({ a: "b" }))
+      .raw();
+    console.log("CreateAccountLog: ", r2);
 
     const r4 = await env.D1_TEST.prepare(
       "SELECT json_extract(data, '$.a') AS a_value FROM account_log WHERE data IS NOT NULL;",
-    ).raw()
-    console.log('GetAccountLogRaw: ', r4)
+    ).raw();
+    console.log("GetAccountLogRaw: ", r4);
 
-    const r3 = await env.D1_TEST.prepare('SELECT * FROM account_log;').all()
-    console.log('ListAccountLogs: ', r3)
+    const r3 = await env.D1_TEST.prepare("SELECT * FROM account_log;").all();
+    console.log("ListAccountLogs: ", r3);
 
-    await db.listAccounts(env.D1_TEST)
+    await db.listAccounts(env.D1_TEST);
 
     const account = await db.getAccount(env.D1_TEST, {
-      id: 'voluntas',
-    })
+      id: "voluntas",
+    });
     if (!account) {
-      return new Response('Not Found', { status: 404 })
+      return new Response("Not Found", { status: 404 });
     }
 
     return new Response(JSON.stringify(account), {
       status: 200,
-      headers: { 'content-type': 'application/json' },
-    })
+      headers: { "content-type": "application/json" },
+    });
   },
-}
+};
--- test/account.test.ts
+++ test/account.test.ts
@@ -1,58 +1,67 @@
-import { D1Database } from '@cloudflare/workers-types/experimental'
-import * as db from '../src/gen/sqlc/querier'
+import { D1Database } from "@cloudflare/workers-types/experimental";
+import * as db from "../src/gen/sqlc/querier";
 
-import { expect, test } from 'vitest'
+import { expect, test } from "vitest";
 
 // shared と d1 は miniflare v2 の遺産なので、そのうち使えなくなる
 // miniflare v3 側テスト用のライブラリを用意してくれるまで焦らずまつしかない
-import { createSQLiteDB } from '@miniflare/shared'
-import { D1Database as miniflareD1Database, D1DatabaseAPI } from '@miniflare/d1'
+import { createSQLiteDB } from "@miniflare/shared";
+import {
+  D1Database as miniflareD1Database,
+  D1DatabaseAPI,
+} from "@miniflare/d1";
 
-test('account', async () => {
-  const sqliteDb = await createSQLiteDB(':memory:')
+test("account", async () => {
+  const sqliteDb = await createSQLiteDB(":memory:");
   // sqlc で生成したコードを使いたいが D1Database の型がマッチしない問題がある
   // miniflare が定義している miniflareD1Database と @cloudflare/workers-types が定義している D1Database の型が異なる
   // そのため D1 を渡そうとするとエラーになる
   // 解決策として @cloudflare/workers-types の D1Database 型に無理矢理変換している
-  const D1 = new miniflareD1Database(new D1DatabaseAPI(sqliteDb)) as D1Database
+  const D1 = new miniflareD1Database(new D1DatabaseAPI(sqliteDb)) as D1Database;
 
   // TODO: ここは本来 db/schema.sql ファイル読み込むようにする
   await D1.exec(
     `CREATE TABLE account (pk INTEGER PRIMARY KEY AUTOINCREMENT, id TEXT UNIQUE NOT NULL, display_name TEXT NOT NULL, email TEXT);`,
-  )
+  );
 
   const r1 = await db.createAccount(D1, {
-    id: 'test',
-    displayName: 'Test',
-    email: 'test@example.com',
-  })
-  expect(r1.success).toBe(true)
+    id: "test",
+    displayName: "Test",
+    email: "test@example.com",
+  });
+  expect(r1.success).toBe(true);
 
-  const a1 = await db.getAccount(D1, { id: 'test' })
-  expect(a1).not.toBeNull()
-  expect(a1?.displayName).toBe('Test')
-  expect(a1?.email).toBe('test@example.com')
+  const a1 = await db.getAccount(D1, { id: "test" });
+  expect(a1).not.toBeNull();
+  expect(a1?.displayName).toBe("Test");
+  expect(a1?.email).toBe("test@example.com");
 
-  const r2 = await db.listAccounts(D1)
-  expect(r2.success).toBeTruthy()
-  expect(r2.results).not.toBeUndefined()
-  expect(r2.results?.length).toBe(1)
-  expect(r2.results?.[0].displayName).toBe('Test')
-  expect(r2.results?.[0].email).toBe('test@example.com')
+  const r2 = await db.listAccounts(D1);
+  expect(r2.success).toBeTruthy();
+  expect(r2.results).not.toBeUndefined();
+  expect(r2.results?.length).toBe(1);
+  expect(r2.results?.[0].displayName).toBe("Test");
+  expect(r2.results?.[0].email).toBe("test@example.com");
 
   const a2 = await db.updateAccountDisplayName(D1, {
-    id: 'test',
-    displayName: 'Test2',
-  })
-  expect(a2).not.toBeNull()
-  expect(a2?.displayName).toBe('Test2')
+    id: "test",
+    displayName: "Test2",
+  });
+  expect(a2).not.toBeNull();
+  expect(a2?.displayName).toBe("Test2");
 
   // batch のテスト
   const r3 = await D1.batch([
-    db.deleteAccount(D1, { id: 'test' }).batch(),
-    db.createAccount(D1, { id: 'test', displayName: 'Test', email: 'test@example.com' }).batch(),
-  ])
+    db.deleteAccount(D1, { id: "test" }).batch(),
+    db
+      .createAccount(D1, {
+        id: "test",
+        displayName: "Test",
+        email: "test@example.com",
+      })
+      .batch(),
+  ]);
 
-  expect(r3[0].success).toBe(true)
-  expect(r3[1].success).toBe(true)
-})
+  expect(r3[0].success).toBe(true);
+  expect(r3[1].success).toBe(true);
+});
