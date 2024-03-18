6191ebc4e2b9112461e0e85a57c2f7ad98162dce
voluntas <nakai@shiguredo.jp>
2024-03-18 20:34:37 +0900

    テスト名を変更する

--- test/miniflarev2.test.ts
+++ test/miniflarev2.test.ts
@@ -1,15 +1,15 @@
-import { D1Database } from "@cloudflare/workers-types/experimental";
+import type { D1Database } from "@cloudflare/workers-types/experimental";
 import * as db from "../src/gen/sqlc/querier";
 
 import { expect, test } from "vitest";
 
-// shared と d1 は miniflare v2 の遺産なので、そのうち使えなくなる
-// miniflare v3 側テスト用のライブラリを用意してくれるまで焦らずまつしかない
-import { createSQLiteDB } from "@miniflare/shared";
 import {
   D1Database as miniflareD1Database,
   D1DatabaseAPI,
 } from "@miniflare/d1";
+// shared と d1 は miniflare v2 の遺産なので、そのうち使えなくなる
+// miniflare v3 側テスト用のライブラリを用意してくれるまで焦らずまつしかない
+import { createSQLiteDB } from "@miniflare/shared";
 
 test("account", async () => {
   const sqliteDb = await createSQLiteDB(":memory:");
@@ -17,33 +17,33 @@ test("account", async () => {
   // miniflare が定義している miniflareD1Database と @cloudflare/workers-types が定義している D1Database の型が異なる
   // そのため D1 を渡そうとするとエラーになる
   // 解決策として @cloudflare/workers-types の D1Database 型に無理矢理変換している
-  const D1 = new miniflareD1Database(new D1DatabaseAPI(sqliteDb)) as D1Database;
+  const d1 = new miniflareD1Database(new D1DatabaseAPI(sqliteDb)) as D1Database;
 
   // TODO: ここは本来 db/schema.sql ファイル読み込むようにする
-  await D1.exec(
-    `CREATE TABLE account (pk INTEGER PRIMARY KEY AUTOINCREMENT, id TEXT UNIQUE NOT NULL, display_name TEXT NOT NULL, email TEXT);`,
+  await d1.exec(
+    "CREATE TABLE account (pk INTEGER PRIMARY KEY AUTOINCREMENT, id TEXT UNIQUE NOT NULL, display_name TEXT NOT NULL, email TEXT);",
   );
 
-  const r1 = await db.createAccount(D1, {
+  const r1 = await db.createAccount(d1, {
     id: "test",
     displayName: "Test",
     email: "test@example.com",
   });
   expect(r1.success).toBe(true);
 
-  const a1 = await db.getAccount(D1, { id: "test" });
+  const a1 = await db.getAccount(d1, { id: "test" });
   expect(a1).not.toBeNull();
   expect(a1?.displayName).toBe("Test");
   expect(a1?.email).toBe("test@example.com");
 
-  const r2 = await db.listAccounts(D1);
+  const r2 = await db.listAccounts(d1);
   expect(r2.success).toBeTruthy();
   expect(r2.results).not.toBeUndefined();
   expect(r2.results?.length).toBe(1);
   expect(r2.results?.[0].displayName).toBe("Test");
   expect(r2.results?.[0].email).toBe("test@example.com");
 
-  const a2 = await db.updateAccountDisplayName(D1, {
+  const a2 = await db.updateAccountDisplayName(d1, {
     id: "test",
     displayName: "Test2",
   });
@@ -51,10 +51,10 @@ test("account", async () => {
   expect(a2?.displayName).toBe("Test2");
 
   // batch のテスト
-  const r3 = await D1.batch([
-    db.deleteAccount(D1, { id: "test" }).batch(),
+  const r3 = await d1.batch([
+    db.deleteAccount(d1, { id: "test" }).batch(),
     db
-      .createAccount(D1, {
+      .createAccount(d1, {
         id: "test",
         displayName: "Test",
         email: "test@example.com",
